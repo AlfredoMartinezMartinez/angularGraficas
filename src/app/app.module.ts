4dae8b0d75cdea80b8c565774e411289165b8f23
AlfredoMartinezMartinez <alfredo.martinez1@alu.uclm.es>
2018-03-17 01:19:43 +0100

    Graficos dinamicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Graficos
+import { ChartsModule } from 'ng2-charts';
+
 
 import { AppComponent } from './app.component';
+import {LineaComponent} from './component/linea/linea.component';
+import { BarraComponent } from './component/barra/barra.component';
+import { DonutComponent } from './component/donut/donut.component';
+import { RadarComponent } from './component/radar/radar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineaComponent,
+    BarraComponent,
+    DonutComponent,
+    RadarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
